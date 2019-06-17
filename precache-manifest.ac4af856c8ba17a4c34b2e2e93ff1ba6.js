b897d8963da845a80fcaf1e2f93f539f9ef345e5
kleber-viana <klebercamvi@yahoo.com.br>
2019-06-17 17:44:59 -0300

    Updates

---
+++ precache-manifest.ac4af856c8ba17a4c34b2e2e93ff1ba6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3d22239483be7a9bbb426d6bade3c1",
+    "url": "/finnsword/repositorio-portifolio.git/index.html"
+  },
+  {
+    "revision": "1935530159c305eaf9b6",
+    "url": "/finnsword/repositorio-portifolio.git/static/css/main.2b6b29e6.chunk.css"
+  },
+  {
+    "revision": "32ba428a442bce32e14e",
+    "url": "/finnsword/repositorio-portifolio.git/static/js/2.249420ef.chunk.js"
+  },
+  {
+    "revision": "1935530159c305eaf9b6",
+    "url": "/finnsword/repositorio-portifolio.git/static/js/main.8c6f4f0e.chunk.js"
+  },
+  {
+    "revision": "ec29f93a9438c16825a6",
+    "url": "/finnsword/repositorio-portifolio.git/static/js/runtime~main.5abea475.js"
+  },
+  {
+    "revision": "a658bc78f80f41313e2868c8c0723d6c",
+    "url": "/finnsword/repositorio-portifolio.git/static/media/curriculo.a658bc78.pdf"
+  },
+  {
+    "revision": "67c7dd19f239ada832224bf30147dc61",
+    "url": "/finnsword/repositorio-portifolio.git/static/media/ellp.67c7dd19.png"
+  },
+  {
+    "revision": "5a376c0c0fb1a59bf5406cdb1d2fab28",
+    "url": "/finnsword/repositorio-portifolio.git/static/media/facebook.5a376c0c.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/finnsword/repositorio-portifolio.git/static/media/git.0ba2aa20.png"
+  },
+  {
+    "revision": "f049e31cad6e2c5d991c10e427ad8570",
+    "url": "/finnsword/repositorio-portifolio.git/static/media/kleber.f049e31c.png"
+  }
+]);
